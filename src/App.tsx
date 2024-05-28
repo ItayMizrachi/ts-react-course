3af4c1125b13836bfe2927e32fbca8ceb0f3d0eb
ItayMizrachi <itaymiz98@gmail.com>
2024-05-29 00:34:54 +0300

    effect start

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,27 @@
-import { useState } from "react";
-import ExpenseList from "./components/expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
-import categories from "./components/expense-tracker/categories";
+import { useEffect, useRef } from "react";
 
 const App = () => {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const ref = useRef<HTMLInputElement>(null);
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 13, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 17, category: "Boat" },
-    { id: 4, description: "ddd", amount: 19, category: "Groceries" },
-    { id: 5, description: "eee", amount: 19, category: "Utilities" },
-  ]);
+  //after each Render
+  useEffect(() => {
+    //Side effect
+    if (ref.current) ref.current.focus();
+  }, []);
 
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  useEffect(() => {
+    document.title = "My App";
+  });
 
   return (
-    <>
-      <div className="mb-5">
-        <h1>Add Expenses</h1>
-        <ExpenseForm
-          onSubmit={(newExpense) =>
-            setExpenses([
-              ...expenses,
-              { ...newExpense, id: expenses.length + 1 },
-            ])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+    <div>
+      <input
+        type="text"
+        ref={ref}
+        className="form-control"
+        placeholder="lorem ipsum"
       />
-    </>
+    </div>
   );
 };
 
