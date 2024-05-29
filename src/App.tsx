d957c95aa380a2850567c1a1571cafd8f9f0d37f
ItayMizrachi <itaymiz98@gmail.com>
2024-05-29 14:02:18 +0300

    loading + DELETE

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,61 @@ interface User {
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [err, setErr] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
     axios
       .get<User[]>(`https://jsonplaceholder.typicode.com/users`, {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setErr(err.message);
+        setLoading(false);
       });
+    // .finally(() => {
+    //   setLoading(false);
+    // }); DOESNT WORK ONLY WITH STRICT MODE
     return () => controller.abort();
   }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    // in case delete fails , saving original users to display the deleted user again
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${user.id}`)
+      .catch((err) => {
+        setErr(err.message);
+        setUsers(originalUsers); //setting users to the original users on FAIL
+      });
+  };
 
   return (
     <>
       {err.length > 1 && <p className="text-danger">{err}</p>}
-      <ul>
+      {isLoading && <div className="spinner-border"></div>}
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            className="d-flex justify-content-between list-group-item"
+            key={user.id}
+          >
+            {user.name}
+            <button
+              onClick={() => deleteUser(user)}
+              className="btn btn-outline-danger"
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
