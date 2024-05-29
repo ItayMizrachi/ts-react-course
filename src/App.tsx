c202246e785f35c3b7f5e9162c52af152b418c36
ItayMizrachi <itaymiz98@gmail.com>
2024-05-29 13:12:17 +0300

    effect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import { useState } from "react";
-import ProductList from "./components/ProductList";
+import { useEffect } from "react";
+
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconnecting");
 
 const App = () => {
-  const [category, setCategory] = useState("");
+  useEffect(() => {
+    connect();
 
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
+    return () => disconnect();
+  }, []);
+
+  return <div>App</div>;
 };
 
 export default App;
